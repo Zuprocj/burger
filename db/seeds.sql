4b5419956336b2c68e55e45532ba7f081503629f
zuprocj <jmdcorpuz@gmail.com>
2019-10-30 15:00:09 -0700

    added db

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Good Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Better Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Best Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royale with Cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Better Burger Jr.', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Teeny Weeny Burger', true);
